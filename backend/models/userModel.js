bfee75329328b3dc1742c3d62e8eba53cfc50938
HarmandeepRattan <harmandeepwd@gmail.com>
2026-03-14 21:42:51 +0530

    create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+const {Schema,model, default: mongoose}=require('mongoose')
+const userSchema =new mongoose.Schema({
+    name:{
+        type:String
+    },
+    email:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String,
+    },
+    profileImage:{
+        type:String,
+        default:""
+    },
+    role:{
+        type:String,
+        enum:["admin","user"],
+        default:"user"
+    }
+},{timestamps:true}
+)
+const user=mongoose.model("User",userSchema)
+module.exports=user
