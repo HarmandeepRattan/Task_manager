cfd1548b2b08b6d2523ac76c6d9a3c1fd2c6f8ad
HarmandeepRattan <harmandeepwd@gmail.com>
2026-03-14 17:12:30 +0530

    tailwind css installiation and setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>Task Manager App</div>
+    <div className='text-red-500 text-3xl underline'>Task Manager App</div>
   )
 }
