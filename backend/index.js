aa3aa0be90d9be3e3e77ca42db8e73fe3b2e7cb0
HarmandeepRattan <harmandeepwd@gmail.com>
2026-03-14 20:20:40 +0530

    middleware to handle CORS

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+// Middleware to handle cors
+const cors = require('cors')
+app.use(cors({
+    origin:process.env.FRONT_END_URL || "http://localhost:5173",
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders: ["Content-Type","Authorization"]
+}))
+
+// Middleware to handle JSON object in req body
+app.use(express.json())
 
 app.listen(8080,()=>{
     console.log("server is running");
